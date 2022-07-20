267d5ba7ed360bd4aa586e0eb3ac8baac6091982
Pierre-Alexandre Murena <pierre-alexandre.murena@aalto.fi>
2022-07-20 11:38:36 +0300

    initial shape analogy code

--- rectangle-analogy.py
+++ rectangle-analogy.py
@@ -25,6 +25,8 @@ from shapes.shape import Shape
 def solve(SA: Shape, SB: Shape, SC: Shape) -> Shape:
     shapelist = (SA, SB, SC)
     
+    solution_decomposition = list()
+    
     # Get inner and outter rectangles
     rs = (s.getInnerRectangle() for s in shapelist)
     Rs = (s.getOutterRectangle() for s in shapelist)
@@ -34,8 +36,12 @@ def solve(SA: Shape, SB: Shape, SC: Shape) -> Shape:
     Rd = Rectangle.analogy(Rs[0], Rs[1], Rs[2])
     
     r = rd.intersect(Rd)
+    solution_decomposition.append(Shape.fromRectangle(r))
     
-    return 0
+    frames = (Rs[i].extractFrame(rs[i]) for i in range(3))
+    
+    solution_decomposition += [solve(frames[0][i], frames[1][i], frames[2][i]) for i in range(4)]
+    return Shape(set.union(*solution_decomposition))
 
 
 
@@ -52,3 +58,13 @@ r = shape.getInnerRectangle()
 
 options = {'seed': 42, 'n_runs': 100}
 shape.getInnerRectangle(method="stochastic", **options)
+
+
+
+
+
+
+R = Rectangle(Point(0,5), 10, 5)
+r = Rectangle(Point(2,3), 4, 2)
+
+print(R.extractFrame(r))
